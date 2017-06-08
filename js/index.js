991e27c6aaa1678c303a453e5695b6f43f411584
sengcen <shenchuan200805@163.com>
2017-06-08 17:25:46 +0800

    add test data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $(function () {
 	$.getJSON("db/notes.json", function (data) {
-       	console.log(data);	
+		var liNode = document.createElement("li");
+		var aNode = document.createElement("a");
+		aNode.class = "wrap-img";
+		aNode.href = "#";
+		liNode.appendChild(aNode);
+		console.log(liNode);
+		$("#note-list").append(liNode);
+       		
     });
 });
